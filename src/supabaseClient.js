7b190313ea40a48c9f522c835cb126b8fe45b343
sedestech <145776658+sedestech@users.noreply.github.com>
2025-06-04 10:43:23 +0200

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://YOUR_PROJECT.supabase.co'; // Remplacez par votre URL
-const supabaseKey = 'YOUR_PUBLIC_ANON_KEY'; // Remplacez par votre clé publique
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
